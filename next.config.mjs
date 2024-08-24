f0bb882f1a4dbc2eab162a2106c40523ca2f88f5
moudy-wold <ahmad.najy5@gmail.com>
2024-08-25 01:36:25 +0300

    e

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        API_BASE_URL: "https://mobilstore.aymankanawi.info",    
-        SITE_KEY: "6Lf8yigqAAAAAOJAK-FeTazsJ3r72g0LcKSiNLHs",
-        RECAPTCHA_SECRET_KEY: "6Lf8yigqAAAAAPKQ-Zxh9DcGVR4mmtd9_xSenjV3",
-      },
-      images: {
-        domains: ["res.cloudinary.com","mobilstore.aymankanawi.info"],
-      },
-      experimental: {
-        serverActions: true,
-      },
-       output:"standalone"
+  env: {
+    API_BASE_URL: "https://mobilstore.aymankanawi.info",    
+    SITE_KEY: "6Lf8yigqAAAAAOJAK-FeTazsJ3r72g0LcKSiNLHs",
+    RECAPTCHA_SECRET_KEY: "6Lf8yigqAAAAAPKQ-Zxh9DcGVR4mmtd9_xSenjV3",
+  },
+  images: {
+    domains: ["res.cloudinary.com","mobilstore.aymankanawi.info"],
+  },
+  experimental: {
+    serverActions: true,
+  },
+   output:"standalone"
 };
 
 export default nextConfig;
